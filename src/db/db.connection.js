2ceeca5755b27a6fd29fb3cd9d24e87b7677a9ae
arjun-saseendran <arjunsaseendran@icloud.com>
2024-11-06 09:15:32 +0530

    add connectDB

--- src/db/db.connection.js
+++ src/db/db.connection.js
@@ -1,2 +1,6 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants";
+
+export const connectDB = async () => {
+  mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import app from "./app.js";
 import dotev from "dotenv";
+import { connectDB } from "./db/db.connection.js";
 
+dotev.config({ path: "./.env" });
 const PORT = process.env.PORT || 3000;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch(() => console.log("MongoDB connection error ", error));
