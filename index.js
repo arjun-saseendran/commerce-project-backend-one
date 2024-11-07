99a5abb26f32ad892fb2cad04dd3ba902e63785b
arjun-saseendran <arjunsaseendran@icloud.com>
2024-11-07 12:19:11 +0530

    config host

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import { app } from "./src/app.js";
 import dotenv from "dotenv";
 import connectDB from "./src/db/db.connection.js";
+import cors from "cors";
 
 dotenv.config({ path: "./.env" });
 const PORT = process.env.PORT || 3000;
-
+const corsOptions = {
+  origin: process.env.CORS || "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  credentials: true,
+};
+app.use(cors(corsOptions));
 connectDB()
   .then(() => {
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
