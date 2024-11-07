68b41929e4b871846b9aa6ea351a69356547ce47
arjun-saseendran <arjunsaseendran@icloud.com>
2024-11-07 12:25:36 +0530

    config host

--- src/app.js
+++ src/app.js
@@ -2,15 +2,8 @@ import express from "express";
 import ProductRouter from "./routes/product.routes.js";
 import UserRouter from "./routes/user.routes.js";
 import cors from "cors";
-import dotenv from "dotenv";
 
 const app = express();
-dotenv.config({ path: "./.env" });
-const corsOptions = {
-  origin: process.env.CORS || "http://localhost:5173",
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-  credentials: true,
-};
 
 app.use(cors(corsOptions));
 app.use(express.json());
