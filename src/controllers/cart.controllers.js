8806e130df0471bc0b8ddea7451516e3b552a396
arjun-saseendran <arjunsaseendran@icloud.com>
2024-11-13 10:32:55 +0530

    add addToCart

--- src/controllers/cart.controllers.js
+++ src/controllers/cart.controllers.js
@@ -3,3 +3,17 @@ import Cart from "../models/cart.models.js";
 const renderCartItems = (req, res) => {
   res.send("All cart items");
 };
+
+const addToCart = (req, res) => {
+  const { user, product, quantity } = req.body;
+  const cartItem = new Cart(user._id, product._id, quantity);
+  cartItem
+    .save()
+    .then((cart) => res.json(cart))
+    .catch((error) => {
+      console.log(error);
+      res.status(400).json(error);
+    });
+};
+
+export { renderCartItems, addToCart };
