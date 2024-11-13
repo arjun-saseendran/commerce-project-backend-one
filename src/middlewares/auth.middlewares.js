89914d26959a3e106f16fe6af74c4c4f95d6eabe
arjun-saseendran <arjunsaseendran@icloud.com>
2024-11-13 10:44:48 +0530

    add authUser

---
+++ src/middlewares/auth.middlewares.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.models.js";
+
+const authUser = async (req, res, next) => {
+  const token = req.header("Authorization");
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      if (decoded.email) {
+        req.user = await User.findOne({ email: decoded.email });
+        next();
+      } else {
+        res.status(401).json({ message: "Invalid token" });
+      }
+    } catch (error) {
+      res.status(401).json({ message: "Invalid token" });
+    }
+  } else {
+    res.status(401).json({ message: "Unauthorized user" });
+  }
+};
+
+export { authUser };
