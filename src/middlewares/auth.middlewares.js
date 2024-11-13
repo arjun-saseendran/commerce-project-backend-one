2d7977341540d0fcf1f7aa575b07ea86e8c581e0
arjun-saseendran <arjunsaseendran@icloud.com>
2024-11-13 16:00:02 +0530

    bug fix

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -1,8 +1,5 @@
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.models.js";
-import dotenv, { configDotenv } from "dotenv";
-
-dotenv.config({ path: "./.env" });
 
 const authUser = async (req, res, next) => {
   const token = req.header("Authorization");
