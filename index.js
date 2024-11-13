51ee85999e756acd73fa62055364ecfba6a3cd67
arjun-saseendran <arjunsaseendran@icloud.com>
2024-11-13 16:20:45 +0530

    bug fix

--- index.js
+++ index.js
@@ -3,17 +3,9 @@ import dotenv from "dotenv";
 import connectDB from "./src/db/db.connection.js";
 dotenv.config({ path: "./.env" });
 const PORT = process.env.PORT || 3000;
-import cors from "cors";
-
-const corsOptions = {
-  origin: [process.env.CORS, "http://localhost:5173"],
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-  credentials: true,
-};
 
 connectDB()
   .then(() => {
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-    app.use(cors(corsOptions));
   })
   .catch((error) => console.log("MongoDB connection error ", error));
--- src/app.js
+++ src/app.js
@@ -2,10 +2,17 @@ import express from "express";
 import ProductRouter from "./routes/product.routes.js";
 import UserRouter from "./routes/user.routes.js";
 import CartRouter from "./routes/cart.routes.js";
+import cors from "cors";
 
 const app = express();
+const corsOptions = {
+  origin: [process.env.CORS, "http://localhost:5173"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  credentials: true,
+};
 
 app.use(express.json());
+app.use(cors(corsOptions));
 
 // routes
 app.use("/product", ProductRouter);
