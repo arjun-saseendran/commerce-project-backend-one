125b9b1e9e79b77a0a216515c6888e8601b547be
arjun-saseendran <arjunsaseendran@icloud.com>
2024-11-15 06:07:00 +0530

    fix cors issue

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ import connectDB from "./src/db/db.connection.js";
 dotenv.config({ path: "./.env" });
 const PORT = process.env.PORT || 3000;
 
+app.use(
+  cors({
+    origin: [
+      process.env.CORS,
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+    ],
+    credentials: true,
+  })
+);
+
 connectDB()
   .then(() => {
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
