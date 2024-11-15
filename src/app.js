128f0412552fd942e894718a4c0eba8ad65732fd
arjun-saseendran <arjunsaseendran@icloud.com>
2024-11-15 09:19:26 +0530

    bug fix

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ import cors from "cors";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: [
+      process.env.CORS,
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 // routes
