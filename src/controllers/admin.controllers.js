bd6db6c0214a1feedf9bf86ee0d4bbffb803a3a0
arjun-saseendran <arjunsaseendran@icloud.com>
2024-11-17 06:05:49 +0530

    add admin.controllers

---
+++ src/controllers/admin.controllers.js
@@ -0,0 +1,19 @@
+import Product from "../models/product.models.js";
+
+const addProduct = (req, res) => {
+  const { title, price, discount, stock } = req.body;
+  const product = {
+    image: `product_images/${req.file.filename}`,
+    title,
+    price,
+    discount,
+    stock,
+  };
+  const newProduct = new Product(product)
+  newProduct.save().then(response => {
+console.log(response);
+res.status(201).json({message: 'Product added succfully'})
+  })
+};
+
+export {addProduct}
