dfce7bc2660b2c3a547205bed6739cc022b03c43
arjun-saseendran <arjunsaseendran@icloud.com>
2024-11-17 07:55:23 +0530

    fix cors

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(
       "http://localhost:5173",
       "http://127.0.0.1:5173",
     ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
 );
--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ app.use(
       "http://localhost:5173",
       "http://127.0.0.1:5173",
     ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
   })
 );
